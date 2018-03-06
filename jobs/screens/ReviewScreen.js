9743d41597aa66a92f267ba28518bca6e3aa55d6
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-03-06 14:27:45 -0500

    added slides components

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -1,19 +1,48 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
+import { navigation, screenProps } from 'react-navigation';
+import { Button } from 'react-native-elements';
+
 
 class ReviewScreen extends Component {
-	render() {
-		return (
-			<View>
-				<Text>ReviewScreen</Text>
-				<Text>ReviewScreen</Text>
-				<Text>ReviewScreen</Text>
-				<Text>ReviewScreen</Text>
-				<Text>ReviewScreen</Text>
-				<Text>ReviewScreen</Text>
-			</View>
-		);
-	}
+	// static navigationOptions = ({navigation, screenProps}) => {
+	// 	return ({
+	// 		title: 'Review Jobs',
+	// 		headerRight: <Button title = "Settings" onPress={ () =>} />
+	// 	});
+	// }
+
+	static navigationOptions = ({ navigation }) => {
+	 	return {
+	     title: 'Review Jobs',
+	     headerRight: (
+	         <Button 
+		         title='Settings' 
+		         onPress={ () => navigation.navigate('settings')} 
+		         backgroundColor = "rgba(0, 0, 0, 0)"
+		         color = "rgba(0, 122, 255, 1)"
+	         />
+	        ),
+	     style: {
+	     	marginTop: Platform.OS === 'android' ? 24 : 0 
+	     }
+		};
+ }
+
+	render () {
+    return (
+      <View>
+      <Text>Review Screen </Text>
+      <Text>Review Screen </Text>
+      <Text>Review Screen </Text>
+      <Text>Review Screen </Text>
+      <Text>Review Screen </Text>
+      </View>
+      );
+  }
 }
 
 export default ReviewScreen;
+
+
+
