815ef46a35a4fe1a6146ece273df6b71c0e3793d
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-03-06 15:12:30 -0500

    initial styling to slide

--- jobs/screens/ReviewScreen.js
+++ jobs/screens/ReviewScreen.js
@@ -14,19 +14,19 @@ class ReviewScreen extends Component {
 
 	static navigationOptions = ({ navigation }) => {
 	 	return {
-	     title: 'Review Jobs',
-	     headerRight: (
-	         <Button 
-		         title='Settings' 
-		         onPress={ () => navigation.navigate('settings')} 
-		         backgroundColor = "rgba(0, 0, 0, 0)"
-		         color = "rgba(0, 122, 255, 1)"
-	         />
-	        ),
-	     style: {
+     title: 'Review Jobs',
+     headerRight: (
+       <Button 
+         title='Settings' 
+         onPress={ () => navigation.navigate('settings')} 
+         backgroundColor = "rgba(0, 0, 0, 0)"
+         color = "rgba(0, 122, 255, 1)"
+       />
+        ),
+     style: {
 	     	marginTop: Platform.OS === 'android' ? 24 : 0 
 	     }
-		};
+	   };
  }
 
 	render () {
