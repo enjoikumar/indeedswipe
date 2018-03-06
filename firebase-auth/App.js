2a470d58f5b80010641dd71bc08e7fb243e43be1
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-03-06 10:49:49 -0500

    authenticated with firebase

--- firebase-auth/App.js
+++ firebase-auth/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import SignUpForm from './components/SignUpForm'
+import firebase from 'firebase';
+import SignUpForm from './components/SignUpForm';
+import SignInForm from './components/SignInForm';
+
+
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyB4Qei31QmygAjD_008TLkwDVOL41vMeYY",
+      authDomain: "one-time-password-91bee.firebaseapp.com",
+      databaseURL: "https://one-time-password-91bee.firebaseio.com",
+      projectId: "one-time-password-91bee",
+      storageBucket: "one-time-password-91bee.appspot.com",
+      messagingSenderId: "641510416427"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <SignUpForm />
+        <SignInForm />
       </View>
     );
   }
@@ -17,6 +34,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
