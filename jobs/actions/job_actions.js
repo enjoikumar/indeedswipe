7f2a7ba281674b2615283c94fab88f4096fb7b68
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-03-08 15:59:39 -0500

    after getting jobs, user is directed to the deck screen

--- jobs/actions/job_actions.js
+++ jobs/actions/job_actions.js
@@ -21,18 +21,19 @@ const buildJobsUrl = (zip) => {
 	return `${JOB_ROOT_URL}${query}`;
 };
 
-export const fetchJobs = (region) => async (dispatch) => {
+export const fetchJobs = (region, callback) => async (dispatch) => {
   try {
     let zip = await reverseGeocode(region);
     const url = buildJobsUrl(zip);
     let { data } = await axios.get(url); //get that list of jobs and return a respond jobs data
     dispatch({ type: FETCH_JOBS, payload: data });
+    callback();
     console.log(data)
   } catch(e) {
     console.error(e);
   }
 }; 
-
+       
 
 
 
--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,97 +1,3 @@
-// import React, { Component } from 'react';
-// import { View, Text, ActivityIndicator } from 'react-native';
-// import { MapView } from 'expo';
-// import { connect } from 'react-redux';
-// import  { Button } from 'react-native-elements';
-
-// import * as actions from '../actions';
-
-// class MapScreen extends Component {
-// 	state = {
-// 		mapLoaded: false,
-// 		region: {
-// 			longitude: -122,
-// 			latitude: 37,
-// 			longitudeDelta: 0.04,
-// 			latitudeDelta: 0.09
-// 		}
-// 	}
-
-// 	componentDidMount() {
-// 		this.setState({ mapLoaded: true });
-// 	}
-
-// 	onRegionChangeComplete = (region) => {  //every single time the user moves
-// 		this.setState({ region })             //around the region state updates
-// 	}
-
-// 	onButtonPress = () => {
-// 		this.props.fetchJobs(this.state.region);
-// 	}
-
-// 	render() {
-// 		if (!this.state.mapLoaded) {
-// 			return (
-// 				<View style = {{ flex: 1, justifyContent: 'center'}}>
-// 					<ActivityIndicator  size = "large" />
-// 				</View>
-// 			)
-// 		}
-		
-// 		return (
-// 			<View style={{ flex: 1 }}>
-// 				<MapView 
-// 					region = {this.state.region}
-// 					style={{ flex: 1 }}
-// 					onRegionChangeComplete = {this.onRegionChangeComplete} 
-// 					/>
-// 				<View style = {styles.buttonContainer}>
-// 					<Button
-// 					large
-// 					title  = "Search This Area" 
-// 					backgroundColor = '#009688'
-// 					icon = {{ name: 'search' }}
-// 					onPress = {this.onButtonPress}
-// 					/>
-// 				</View>
-// 			</View>
-// 		);
-// 	}
-// }
-
-// const styles = {
-// 	buttonContainer :{
-// 		position: 'absolute',
-// 		bottom: 20,
-// 		left: 0,
-// 		right: 0
-// 	}
-// }
-
-// export default connect(null, actions)(MapScreen);
-
-
-
-// Nyc map region
-// Object {
-//   "latitude": 40.80330997024359,
-//   "latitudeDelta": 0.5482394756968034,
-//   "longitude": -73.85496856041367,
-//   "longitudeDelta": 0.454181483722536,
-// }
-
-
-
-//FOR INDEED APIß
-
-// http://api.indeed.com/ads/apisearch?publisher=4201738803816157&q=java&l=austin&v=2
-
-// Key things to include is:
-// publisher=4201738803816157     AND
-// v=2
-
-
-
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
@@ -165,3 +71,25 @@ const styles = {
 }
 
 export default connect(null, actions)(MapScreen);
+
+
+// Nyc map region
+// Object {
+//   "latitude": 40.80330997024359,
+//   "latitudeDelta": 0.5482394756968034,
+//   "longitude": -73.85496856041367,
+//   "longitudeDelta": 0.454181483722536,
+// }
+
+
+
+//FOR INDEED APIß
+
+// http://api.indeed.com/ads/apisearch?publisher=4201738803816157&q=java&l=austin&v=2
+
+// Key things to include is:
+// publisher=4201738803816157     AND
+// v=2
+
+
+
