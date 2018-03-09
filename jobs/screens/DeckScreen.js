0f428adfa48b58f96db3e9eb43a46fefb2d8ad3c
Kamal Kumar <kamalkumar@Kamals-MacBook-Pro.local>
2018-03-09 12:05:25 -0500

    added platform and cards render a better version of the map

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 import { connect } from 'react-redux';
 import { MapView} from 'expo';
 import { Card, Button } from 'react-native-elements'
@@ -7,12 +7,21 @@ import Swipe from '../components/Swipe';
 
 class DeckScreen extends Component {
 	renderCard(job) {
+		const initialRegion = {
+			longitude: job.longitude,
+			latitude: job.latitude,
+			latitudeDelta: 0.045,
+			longitudeDelta: 0.02
+		};
+
 		return (
 			<Card title = {job.jobtitle}>
 				<View style={{ height: 300}}>
 					<MapView
 					scrollEnabled = {false}
-					style={{ flex: 1 }}
+					style = {{ flex: 1 }}
+					cacheEnabled = {Platform.OS === 'android' ? true : false}
+					initialRegion = {initialRegion}
 					>
 					</MapView>
 				</View>
